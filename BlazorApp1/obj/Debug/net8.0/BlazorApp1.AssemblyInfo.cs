b146b440126bca1325dfb31520a0f63977b99026
Douglas Garcia Rangel <109640187+Douglas4GR@users.noreply.github.com>
2024-05-25 22:33:45 -0300

    incluindo a pagina de meus anuncios

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfa28a5b3b6ac2c8bb35737185b728f8aea6b81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+616e267bda12dcdebb490046c2de536fbaae8377")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
